66a85d18317315c6eedab9e04864c8836a403f3f
Tiger Nie <tnie@tuta.io>
2022-05-16 20:51:37 -0500

    instrument flask app with prometheus

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import time
 import secrets
-from io import BytesIO
 
 from flask import (
     Flask,
@@ -15,6 +14,8 @@ from flask import (
     make_response,
 )
 from werkzeug.utils import secure_filename
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from prometheus_client import make_wsgi_app, Summary
 
 import gangster
 import cv2
@@ -29,41 +30,55 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.config["MAX_CONTENT_LENGTH"] = 5 * 1024 * 1024  # Limit upload size to 5mb
 app.secret_key = secrets.token_urlsafe(32)
 
+# Add prometheus wsgi middleware to route /metrics requests
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
+
+# prometheus metrics
+s_round_trip = Summary(
+    "gangsterify_request_latency_s", "Round trip time to gangsterify an image"
+)
+s_cv = Summary("gangsterify_cv_time_s", "Compute time to gangsterify an image")
+gangster.make_gangster = s_cv.time()(gangster.make_gangster)
+
 
 def allowed_file(filename):
     ext = os.path.splitext(filename)[-1].lower()[1:]  # get rid of .
     return ext in ALLOWED_EXTENSIONS
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.get("/")
+def index():
+    return render_template("index.html")
+
+
+@app.post("/")
+@s_round_trip.time()
 def upload_file():
-    if request.method == "POST":
-        # check if the post request has the file part
-        if "file" not in request.files:
-            flash("No file part")
-            return redirect(request.url)
-        file = request.files["file"]
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if not file.filename:
-            flash("No selected file")
-            return redirect(request.url)
+    # check if the post request has the file part
+    if "file" not in request.files:
+        flash("No file part")
+        return redirect(request.url)
+    file = request.files["file"]
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if not file.filename:
+        flash("No selected file")
+        return redirect(request.url)
 
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
 
-            filename = f"tmp_{int(1000*time.time())}{os.path.splitext(filename)[-1]}"
-            filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-            file.save(filepath)
+        filename = f"tmp_{int(1000*time.time())}{os.path.splitext(filename)[-1]}"
+        filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+        file.save(filepath)
 
-            img = cv2.imread(filepath)
-            n_faces = gangster.make_gangster(img)
-            cv2.imwrite(filepath, img)
-            res_url = url_for("uploaded_file", filename=filename)
-            print(f"{res_url=}, {n_faces=}")
+        img = cv2.imread(filepath)
+        n_faces = gangster.make_gangster(img)
+        cv2.imwrite(filepath, img)
+        res_url = url_for("uploaded_file", filename=filename)
+        print(f"{res_url=}, {n_faces=}")
 
-            return render_template("index.html", res_url=res_url, n_faces=n_faces)
-
+        return render_template("index.html", res_url=res_url, n_faces=n_faces)
     return render_template("index.html")
 
 
